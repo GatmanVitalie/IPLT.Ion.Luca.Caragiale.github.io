fa090d7774cc1161c29071163e07c63eaf3b255c
GatmanVitalie <gatman.vitalie2019@gmail.com>
2023-12-23 18:14:01 +0200

    Am schimba antetul

--- functions.js
+++ functions.js
@@ -1,3 +1,19 @@
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('Antet');
+    if (window.scrollY > 40) {
+        header.style.height = 3 + 'cm';
+        header.style.backgroundColor = '#000'; // Change background color
+        header.style.color = '#fff'; // Change text color
+        // Add any other style changes you want for the scrolled state
+    } else {
+        header.style.backgroundColor = '#FFFAFA'; // Default background color
+        header.style.color = '#333'; // Default text color
+        // Reset any other styles you changed
+    }
+});
+
+
+
 function changeLanguage(lang) {
     if (lang === 'en') {
         document.getElementById('title-en').style.display = 'block';
@@ -10,5 +26,5 @@ function changeLanguage(lang) {
         document.getElementById('content-en').style.display = 'none';
         document.getElementById('content-es').style.display = 'block';
     }
-    
+
 }
