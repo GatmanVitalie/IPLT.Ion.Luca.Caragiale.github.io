c73f13de667a8849db50d3c6a4c1f1610105bb12
GatmanVitalie <gatman.vitalie2019@gmail.com>
2024-01-17 22:19:54 +0200

    Meniu ciotki

--- js/antet.js
+++ js/antet.js
@@ -17,12 +17,7 @@
 var screenWidthInCm = window.innerWidth / window.devicePixelRatio * 2.54 / 96;
 let hamburgerButon = document.getElementById('HamburgerButton');
 
-window.addEventListener('load', function () {
 
-    resize();
-
-
-});
 
 function resize() {
 
@@ -42,16 +37,93 @@ function resize() {
     console.log(window.innerWidth / document.getElementById('dpi').offsetWidth);
 }
 
+function arange() {
+
+    document.getElementById('admitere_butons').style.left = document.getElementById('admitere_button').getBoundingClientRect().left + 'px';
+    document.getElementById('admitere_butons').style.top = document.getElementById('admitere_button').getBoundingClientRect().bottom - 1 + 'px';
+    document.getElementById('admitere_butons').style.width = document.getElementById('admitere_button').getBoundingClientRect().width + 'px';
+
+    document.getElementById('elevi_butons').style.left = document.getElementById('elevi_buton').getBoundingClientRect().left + 'px';
+    document.getElementById('elevi_butons').style.top = document.getElementById('elevi_buton').getBoundingClientRect().bottom - 1 + 'px';
+    document.getElementById('elevi_butons').style.width = document.getElementById('elevi_buton').getBoundingClientRect().width + 'px';
+
+}
+
+
+// Admitere //
+var admitere_active = false;
+function activate_admitere() {
+    document.getElementById('admitere_butons').classList.add('active');
+    admitere_active = true;
+}
+
+function deactivate_admitere() {
+    document.getElementById('admitere_butons').classList.remove('active');
+    admitere_active = false;
+}
+
+function toggle_active_admitere() {
+    if (admitere_active) {
+        document.getElementById('admitere_butons').classList.remove('active');
+        admitere_active = false;
+    }
+    else {
+        document.getElementById('admitere_butons').classList.add('active');
+        admitere_active = true;
+    }
+}
+
+// Elevi //
+var elevi_active = false;
+function activate_elevi() {
+   
+    document.getElementById('elevi_butons').classList.add('active');
+    elevi_active = true;
+}
+
+function deactivate_elevi() {
+    document.getElementById('elevi_butons').classList.remove('active');
+    elevi_active = false;
+}
+
+function toggle_active_elevi() {
+    if (elevi_active) {
+        document.getElementById('elevi_butons').classList.remove('active');
+        elevi_active = false;
+    }
+    else {
+        document.getElementById('elevi_butons').classList.add('active');
+        elevi_active = true;
+    }
+}
+
+window.addEventListener('click', function (e) {
+    if (!((document.getElementById('admitere_butons').contains(e.target)) || (document.getElementById('admitere_button').contains(e.target)))) {
+        document.getElementById('admitere_butons').classList.remove('active');
+    }
+    else if (!((document.getElementById('elevi_butons').contains(e.target)) || (document.getElementById('elevi_buton').contains(e.target)))) {
+        document.getElementById('elevi_butons').classList.remove('active');
+    }
+});
+
+window.addEventListener('load', function () {
+
+    resize();
+    arange();
+
+});
+
 window.onload = function () {
 
     resize();
+    arange();
 
 };
 
 
 window.addEventListener("resize", function () {
     resize();
-
+    arange();
 });
 
 var burger_butons = document.getElementById("burger_butons");
