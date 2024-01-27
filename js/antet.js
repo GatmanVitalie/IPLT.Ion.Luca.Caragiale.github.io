aadc8d9838756972fe94ee76980e74f43007ea3a
GatmanVitalie <gatman.vitalie2019@gmail.com>
2024-01-27 17:22:56 +0200

    schimbari minore

--- js/antet.js
+++ js/antet.js
@@ -23,16 +23,18 @@ function resize() {
 
     screenWidthInCm = window.innerWidth / document.getElementById('dpi').offsetWidth;
     //console.log('resize', screenWidthInCm);
-    if (screenWidthInCm < 13) {
+    if (screenWidthInCm < 14) {
         hamburgerButon.style.display = "block";
         document.getElementById('contcatns_container').style.display = "flex";
         document.getElementById('burger_butons').style.display = "flex";
         document.getElementById('dreapta').style.display = "none";
+        document.getElementById('Antet_Imob').style.alignItems = "center";
     } else {
         hamburgerButon.style.display = "none";
         document.getElementById('contcatns_container').style.display = "none";
         document.getElementById('burger_butons').style.display = "none";
         document.getElementById('dreapta').style.display = "flex";
+        document.getElementById('Antet_Imob').style.alignItems = "flex-end";
     }
     console.log(window.innerWidth / document.getElementById('dpi').offsetWidth);
 }
