8cd40c605078d5214ee3e8598325a75020f79f72
GatmanVitalie <gatman.vitalie2019@gmail.com>
2024-02-18 18:18:12 +0200

    modificat linkuri

--- js/button_hub.js
+++ js/button_hub.js
@@ -5,12 +5,12 @@ function admitere()
 
 function news()
 {
-    window.location.href = "/noutati.html";
+    window.location.href = "./noutati.html";
 }
 
 function about()
 {
-    window.location.href = "/despre.html";
+    window.location.href = "./despre.html";
 }
 
 function contacts()
